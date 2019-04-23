f65128eadfcc6c68ad21d23e963a2efe3e212d72
phess110 <38021866+phess110@users.noreply.github.com>
2019-04-23 17:14:06 -0400

    Add files via upload

---
+++ period.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <vector> 
+#include <cmath>
+
+using namespace std;
+
+/*
+Given a function f: {0,...,m-1} -> {0,...,m-1}, and a seed X_0,
+Let X_i = f(X_(i-1)) for i >= 1.
+
+This function computes the smallest µ such that X_0,...,X_(µ-1) are distinct, but X_µ is equal to X_r for some r < µ.
+as well as computes p such that X_µ = X_(u+kp) for all k >= 0. That is, f is p-periodic starting at µ given seed X_0.
+
+Memory: O(1) 
+Time: O(µ+p)
+*/
+vector<int> findPeriod(int (*f)(int), int seed){
+
+	vector<int> period (2);
+	int n = 1;
+	int v1 = f(seed); //X_n
+	int v2 = f(v1);	//X_2n
+
+	/* Compute smallest n > 0 such that X_n = X_2n */
+	while(true){
+		if(v1 == v2){ break; }
+		v1 = f(v1);
+		v2 = f(f(v2));
+		n++;
+	}
+
+	v2 = v1; // v2 = X_n
+	v1 = seed; // v1 = X_0
+	int i = 0, v3 = v2, p = n; //v3 = X_n and p = n
+
+	/* 
+	Compute smallest i such that v1 = X_i = X_(n+i) = v2.
+	p is the smallest i, 0 < i < µ, such that X_n = X_n+i. 
+	*/
+	while(true){
+		if(v1 == v2){ break; }
+		if( i > 0 && v2 == v3 && p == n){ p = i; } 
+		v1 = f(v1);
+		v2 = f(v2);
+		i++;
+	}
+
+	period[0] = i; //µ = i
+	period[1] = p; //period
+
+	return period;
+}
+
+int main(){
+
+	auto mod = [](int i) { return (5*i+13) % 99; }; // lambda which defines the recursive sequence
+
+	vector<int> v = findPeriod(mod, 5);
+
+	cout << "Period: " << v[1] << "\n";
+	cout << "Starts at: " << v[0];
+
+}
